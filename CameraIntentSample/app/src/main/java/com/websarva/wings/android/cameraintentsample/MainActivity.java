76c3e1e9dd477bffbce59e21364dc0d412448c72
mitaken224 <97575462+mitaken224@users.noreply.github.com>
2022-06-08 13:55:58 +0900

    Initial commit

---
+++ CameraIntentSample/app/src/main/java/com/websarva/wings/android/cameraintentsample/MainActivity.java
@@ -0,0 +1,43 @@
+package com.websarva.wings.android.cameraintentsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // 親クラスの同名メソッドの呼び出し
+        super.onActivityResult(requestCode, resultCode, data);
+        // カメラアプリとの連携からの戻りでかつ撮影成功の場合
+        if(requestCode == 200 && resultCode == RESULT_OK) {
+            // 撮影された画像のビットマップデータを取得
+            Bitmap bitmap = data.getParcelableExtra("data");
+            // 画像を表示するImageViewを取得
+            ImageView ivCamera = findViewById(R.id.ivCamera);
+            // 撮影された画像をImageViewに設定
+            ivCamera.setImageBitmap(bitmap);
+        }
+
+    }
+
+    // 画像部分がタップされたときの処理メソッド
+    public void onCameraImageClick(View view) {
+        // Intentオブジェクトを生成
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // アクティビティを起動
+        startActivityForResult(intent, 200);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# android_app_text
+ 「Androidアプリ開発の教科書」の学習用
